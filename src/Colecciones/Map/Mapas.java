e9c8699de16038bdc7e55484b5d7eb752d963a41
Lauta Di Salvo <lautarodisalvo2023@gmail.com>
2024-06-18 16:00:48 -0300

    algunos agregados

--- src/Colecciones/Map/Mapas.java
+++ src/Colecciones/Map/Mapas.java
@@ -1,11 +1,14 @@
 package Colecciones.Map;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.LinkedHashMap;
+import java.util.HashMap;
+import java.util.TreeMap;
 
 public class Mapas {
     public static void main(String[] args) {
-
-        Map<String, Double> frutas = new HashMap<>();  
+        
+        // Map<String, Double> frutas = new HashMap<>();  
+        Map<String, Double> frutas = new LinkedHashMap<>();  
 
 
         // en los casos de los mapas, las llaves/key son Peras y manzanas
@@ -14,13 +17,15 @@ public class Mapas {
 
         frutas.put("Manzanas", 4.5);
         frutas.put("Peras", 5.23);
-        System.out.println(frutas);
+        frutas.put("Zandias", 5.56);
+
+        
         // KeySet me da un arreglo con las llaves
         for (String e : frutas.keySet()) {
             // con los get() se pueden obtener los valores
             System.out.println(e + " " + frutas.get(e) );
         }
 
-        System.out.println(frutas.containsKey("Manzanas"));
+        // System.out.println(frutas.containsKey("Manzanas"));
     }
 }   
